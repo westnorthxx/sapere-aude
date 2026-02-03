3df5692cd32fecfb50475669b4ddbd96f0cdad06
westnorthxx <zhouxn@cau.edu.cn>
2026-02-03 15:15:23 +0800

    add: footer

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,13 +13,26 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
+        {/* 新增的版权声明部分 */}
+        <p>
+          © {year} <b>westnorth</b>. 保留所有权利。
+        </p>
+        
+        {/* 新增的邮箱联系方式 */}
+        <p>
+          联系邮箱：<a href="mailto:westnorth_xx@163.com">westnorth_xx@163.com</a>
+        </p>
+
+        {/* 原有的 Quartz 署名 */}
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a>
         </p>
+
+        {/* 原有的自定义链接列表 */}
         <ul>
           {Object.entries(links).map(([text, link]) => (
-            <li>
+            <li key={text}>
               <a href={link}>{text}</a>
             </li>
           ))}
